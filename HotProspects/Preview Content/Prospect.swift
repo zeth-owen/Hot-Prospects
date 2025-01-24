848dac545c7032645a1726ddc3d102dffc9841b0
Zeth Thomas <zeththomas89@gmail.com>
2025-01-24 13:35:26 -0800

    first commit

---
+++ HotProspects/Preview Content/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Zeth Thomas on 1/21/25.
+//
+
+import Foundation
---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,21 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Zeth Thomas on 1/21/25.
+//
+
+import SwiftData
+
+@Model
+class Prosepect {
+    var name: String
+    var emailAdress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAdress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAdress = emailAdress
+        self.isContacted = isContacted
+    }
+}
